6d5a6445d3d2c023d687d4f2534d6340980ad683
diegodc <diego.dc.84@hotmail.com>
2017-04-05 20:11:07 -0300

    Package structure changes

--- src/test/java/enigma/core/RotorFactoryTest.java
+++ src/test/java/enigma/core/RotorFactoryTest.java
@@ -1,12 +1,11 @@
 package enigma.core;
 
-import enigma.core.RotorFactory;
 import enigma.core.devices.Rotor;
 import enigma.core.util.Alphabet;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * RotorFactoryTest.

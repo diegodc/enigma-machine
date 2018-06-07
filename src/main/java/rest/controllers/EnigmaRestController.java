8855101dc7a3c883ff10d4019d9e654f5726666d
Diego De Cristofano <diego.dc.84@hotmail.com>
2018-06-07 03:39:37 -0300

    heroku autodeploy test

--- src/main/java/rest/controllers/EnigmaRestController.java
+++ src/main/java/rest/controllers/EnigmaRestController.java
@@ -5,6 +5,7 @@ import enigma.service.EnigmaService;
 import enigma.service.MessageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class EnigmaRestController {
     public CipheredMessage cipherMessage(@RequestBody MessageRequest request) {
 
         return enigmaService.cipherMessage(request);
+    }
+    
+    @GetMapping
+    public String get() {
+        return "Enigma Machine Service";
     }
 
 }
